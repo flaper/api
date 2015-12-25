acd847500c732cce528ca7264a9428dde41c10ab
sklyukin <stas.msu@gmail.com>
2015-12-25 23:26:15 +0500

    markdown images group

--- src/libs/markdown/markdown.js
+++ src/libs/markdown/markdown.js
@@ -3,6 +3,9 @@ import {ImageService} from '../../common/services/ImageService';
 
 //to remove auto ids for headers
 let renderer = new marked.Renderer();
+
+export const MD_IMAGES_GROUP_CLASS="md-images-group";
+
 renderer.heading = function (text, level) {
   return `<h${level}>${text}</h${level}>`;
 };
@@ -14,10 +17,39 @@ renderer.image = function (href, title, text) {
   return parentImageMethod(href, title, text);
 };
 
-marked.setOptions({renderer});
+marked.setOptions({
+  renderer: renderer,
+  breaks: true
+});
+
+//images group should be in separate paragraph
+function imagesToSeparateBlock(markdown) {
+  //select groups of ![title](src)
+  let imgGroupRegex = /((?:!\[[^\[\]]*]\([^\(\)]*\)\s*)+)/g;
+  //let result = markdown.match(imgGroupRegex);
+  return markdown.replace(imgGroupRegex, '\n\n$1\n\n');
+}
+function responsiveImages(html) {
+  //future notes, we can remove <p></p> wrapper from images in future
+  /**
+   * capture group from 1 to 4 images
+   *  ?: means not-capturing group
+   * @type {RegExp}
+   */
+  let imgGroupRegex = /((?:<img [^>]+"> *(?:<br>)? *){1,4})/g;
+
+  //Right now we have to place each image inside a block element due to a
+  //Firefox quirk with scaling flexbox images; hopefully this will be fixed soon.
+  let imgRegex = /(<img [^>]+">)/g;
+  //let result = html.match(imgGroupRegex);
+  html = html.replace(imgGroupRegex, `<div class="${MD_IMAGES_GROUP_CLASS}">$1</div>`)
+    .replace(imgRegex, '<div>$1</div>');
+
+  return html;
+}
 
 export class FlaperMark {
   static toHTML(value) {
-    return marked(value);
+    return responsiveImages(marked(imagesToSeparateBlock(value)));
   }
 }
--- src/test/libs/markdown/markdown.spec.js
+++ src/test/libs/markdown/markdown.spec.js
@@ -1,18 +1,38 @@
 let should = require('chai').should();
 import {TEST_IMAGE_ID, ImageService} from '../../../common/services/ImageService';
-import {FlaperMark} from '../../../libs/markdown/markdown';
+import {FlaperMark, MD_IMAGES_GROUP_CLASS} from '../../../libs/markdown/markdown';
+
+const GROUP_CLASS = MD_IMAGES_GROUP_CLASS;
 
 describe('/markdown', () => {
   it('external image', () => {
     //external means not an image created via Image model
     let html = FlaperMark.toHTML(`![](/external.jpg)`);
-    html.trim().should.eq(`<p><img src="/external.jpg" alt=""></p>`);
+    html.trim().should.eq(`<p><div class="${GROUP_CLASS}"><div><img src="/external.jpg" alt=""></div></div></p>`);
   });
 
   it('local image', () => {
     let id = TEST_IMAGE_ID;
     let html = FlaperMark.toHTML(`![](${id})`);
     let bucket = ImageService.getBucketPath();
-    html.trim().should.eq(`<p><img src="${bucket}${id}.jpg" alt=""></p>`);
+    html.trim().should.eq(`<p><div class="${GROUP_CLASS}"><div><img src="${bucket}${id}.jpg" alt=""></div></div></p>`);
+  });
+
+  it('group images', () => {
+    let markdown = `![](${TEST_IMAGE_ID})`
+    for (let i = 0; i < 7; i++) {
+      markdown += ` ![](/external${i})`;
+    }
+    let result = `<p><div class="${GROUP_CLASS}"><div>` +
+      '<img src="https://s3.eu-central-1.amazonaws.com/flaper.test.images/1abc0000fffffff.jpg" alt=""></div> ' +
+      '<div><img src="/external0" alt=""></div> <div><img src="/external1" alt=""></div> ' +
+      '<div><img src="/external2" alt="">' +
+      `</div> </div><div class="${GROUP_CLASS}"><div><img src="/external3" alt=""></div> ` +
+      '<div><img src="/external4" alt=""></div> ' +
+      '<div><img src="/external5" alt=""></div> <div><img src="/external6" alt=""></div></div></p>';
+
+
+    let html = FlaperMark.toHTML(markdown);
+    html.trim().should.eq(result);
   })
 });
