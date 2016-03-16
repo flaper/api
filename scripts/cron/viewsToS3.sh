fb661a409afde31a76c38335f7f7ba38dd7d4250
sklyukin <stas.msu@gmail.com>
2016-03-16 15:58:19 +0500

    junk path

--- scripts/cron/viewsToS3.sh
+++ scripts/cron/viewsToS3.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-zip  /var/log/flaper/view/`date +\%F`.zip /var/log/flaper/view/view.log && rm /var/log/flaper/view/view.log
+zip  /var/log/flaper/view/`date +\%F`.zip /var/log/flaper/view/view.log -j && rm /var/log/flaper/view/view.log
 aws s3 cp /var/log/flaper/view/`date +\%F`.zip s3://flaper.views/logs/
