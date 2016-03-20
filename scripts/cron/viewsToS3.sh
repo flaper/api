d28a7c28fc864fcfa872b187b65ee107b7c54b4b
sklyukin <stas.msu@gmail.com>
2016-03-20 07:37:03 +0500

    fix log path

--- scripts/cron/viewsToS3.sh
+++ scripts/cron/viewsToS3.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 file_name="$(date -d'yesterday' +\%F).zip"
 zip  /var/log/flaper/view/${file_name} /var/log/flaper/view/view.log -j && rm /var/log/flaper/view/view.log
-/usr/local/bin/aws s3 cp /var/log/flaper/view/${file_name} s3://flaper.views/logs2/${file_name}
+/usr/local/bin/aws s3 cp /var/log/flaper/view/${file_name} s3://flaper.views/logs/${file_name}
