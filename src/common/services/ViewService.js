c48c95be48e48da350471a8f8aac12b50b149ee3
sklyukin <stas.msu@gmail.com>
2016-03-28 08:22:02 +0500

    skip view log for test env

--- src/common/services/ViewService.js
+++ src/common/services/ViewService.js
@@ -5,6 +5,9 @@ import _ from 'lodash';
 export let VIEW_LOG_PATH = '/var/log/flaper/view/view.log';
 export class ViewService {
   static logView(d) {
+    if (App.isTestEnv()) {
+      return Promise.resolve();
+    }
     return new Promise((resolve, reject) => {
       let time = Math.round((new Date()).getTime() / 1000);
       let data = Object.assign({time: time}, d);
--- src/server/boot/logs.js
+++ src/server/boot/logs.js
@@ -2,19 +2,22 @@ import fs from 'fs';
 import path from 'path';
 import mkdirp from 'mkdirp';
 import {VIEW_LOG_PATH} from '../../common/services/ViewService'
+import {App} from '../../common/services/App'
 
 module.exports = function (server) {
-  let dir = path.dirname(VIEW_LOG_PATH);
-  fs.stat(dir, (err, access) => {
-    if (err) {
-      if (err.code === 'ENOENT') {
-        //no such file
-        mkdirp(dir, (err) => {
-          if (err) console.error(err);
-        });
-      } else {
-        console.log(err);
+  if (!App.isTestEnv()) {
+    let dir = path.dirname(VIEW_LOG_PATH);
+    fs.stat(dir, (err, access) => {
+      if (err) {
+        if (err.code === 'ENOENT') {
+          //no such file
+          mkdirp(dir, (err) => {
+            if (err) console.error(err);
+          });
+        } else {
+          console.log(err);
+        }
       }
-    }
-  })
+    })
+  }
 };
