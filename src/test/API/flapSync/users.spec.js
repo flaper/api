f0f110161d8b6253d2207107114c01f1e9e89142
sklyukin <stas.msu@gmail.com>
2016-08-29 12:07:06 +0500

    facebook skipped test

--- src/test/API/flapSync/users.spec.js
+++ src/test/API/flapSync/users.spec.js
@@ -57,4 +57,12 @@ describe(`/${COLLECTION_URL}`, function () {
     users.length.should.eq(1);
     users[0].id.toString().should.eq(USERS.aigul.id)
   });
+
+  it.skip('Sync should find existed facebook user', function*() {
+    let id = 6972616;
+    yield (Flap.syncUser(id));
+    let users = yield (User.find({where: {flapIds: id}}));
+    users.length.should.eq(1);
+    users[0].id.toString().should.eq(USERS.yuriy.id)
+  });
 });
