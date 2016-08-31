49775c6b736245e3665138ece0b97e9fbc8a35a8
sklyukin <stas.msu@gmail.com>
2016-08-31 21:12:25 +0500

    convert test to generators

--- src/test/API/comment/createAndWrite.spec.js
+++ src/test/API/comment/createAndWrite.spec.js
@@ -25,12 +25,11 @@ describe(`/${COLLECTION_URL}/POST&PUT`, function () {
       .expect(401)
   });
 
-  it('User - subjectId should be provided', () => {
-    return user1Promise.then(({agent}) => {
-      return agent.post(COLLECTION_URL)
-        .send(_.omit(NEW_COMMENT, 'subjectId'))
-        .expect(400)
-    })
+  it('User - subjectId should be provided', function*() {
+    let {agent} = yield (user1Promise);
+    yield (agent.post(COLLECTION_URL)
+      .send(_.omit(NEW_COMMENT, 'subjectId'))
+      .expect(400));
   });
 
   it('User - subjectId should exist', function* () {
@@ -40,62 +39,53 @@ describe(`/${COLLECTION_URL}/POST&PUT`, function () {
       .expect(400));
   });
 
-  it('User - allow to add', () => {
+  it('User - allow to add for a story', function *() {
     let created = 0;
-    let commentsNumber = 0;
-    return Story.findByIdRequired(NEW_COMMENT.subjectId)
-      .then(story => commentsNumber = story.commentsNumber)
-      .then(() => {
-        return user1Promise.then(({agent}) => {
-          return agent.post(COLLECTION_URL)
-            .send(NEW_COMMENT)
-            .expect(200)
-            .expect((res) => {
-              let comment = res.body;
-              user1.id.should.equal(comment.userId);
-              NEW_COMMENT.subjectId.should.eq(comment.subjectId);
-              'story'.should.eq(comment.subjectType);
-              comment.content.should.eq(NEW_COMMENT.content);
-              comment.contentHTML.should.eq('test <strong>comment</strong>');
-              Comment.STATUS.ACTIVE.should.equal(comment.status);
-              created = new Date(comment.created);
-            })
-        })
-      })
-      .then(() => Story.findByIdRequired(NEW_COMMENT.subjectId))
-      .then(story => {
-        story.commentsNumber.should.eq(commentsNumber + 1);
-        story.lastActive.getTime().should.eq(created.getTime());
-      })
+    let story = yield (Story.findByIdRequired(NEW_COMMENT.subjectId));
+    let commentsNumber = story.commentsNumber;
+    let {agent} = yield (user1Promise);
+    yield (agent.post(COLLECTION_URL)
+      .send(NEW_COMMENT)
+      .expect(200)
+      .expect((res) => {
+        let comment = res.body;
+        user1.id.should.equal(comment.userId);
+        NEW_COMMENT.subjectId.should.eq(comment.subjectId);
+        'story'.should.eq(comment.subjectType);
+        comment.content.should.eq(NEW_COMMENT.content);
+        comment.contentHTML.should.eq('test <strong>comment</strong>');
+        Comment.STATUS.ACTIVE.should.equal(comment.status);
+        created = new Date(comment.created);
+      }));
+    story = yield (Story.findByIdRequired(NEW_COMMENT.subjectId));
+    story.commentsNumber.should.eq(commentsNumber + 1);
+    story.lastActive.getTime().should.eq(created.getTime());
   });
 
-  it('User (strange) - deny update', () => {
-    return user2Promise.then(({agent}) => {
-      return agent.put(`${COLLECTION_URL}/${NEW_COMMENT.id}`)
-        .send({})
-        .expect(401)
-    })
+  it('User (strange) - deny update', function* () {
+    let {agent} = yield (user2Promise);
+    return yield (agent.put(`${COLLECTION_URL}/${NEW_COMMENT.id}`)
+      .send({})
+      .expect(401));
   });
 
-  it('User (owner) - allow update', () => {
-    return user1Promise.then(({agent}) => {
-      return agent.put(`${COLLECTION_URL}/${NEW_COMMENT.id}`)
-        .send({content: '*new text*'})
-        .expect(200)
-        .expect(res => {
-          let comment = res.body;
-          comment.contentHTML.should.eq('<em>new text</em>')
-        })
-    })
+  it('User (owner) - allow update', function* () {
+    let {agent} = yield (user1Promise);
+    yield (agent.put(`${COLLECTION_URL}/${NEW_COMMENT.id}`)
+      .send({content: '*new text*'})
+      .expect(200)
+      .expect(res => {
+        let comment = res.body;
+        comment.contentHTML.should.eq('<em>new text</em>')
+      }));
   });
 
-  it('User - ignore id update', () => {
+  it('User - ignore id update', function* () {
     let NEW_ID = '1a5000000000000000010002';
-    return user1Promise.then(({agent}) => {
-      return agent.put(`${COLLECTION_URL}/${NEW_COMMENT.id}`)
-        .send({id: NEW_ID})
-        .expect(400)
-    })
+    let {agent} = yield (user1Promise);
+    yield (agent.put(`${COLLECTION_URL}/${NEW_COMMENT.id}`)
+      .send({id: NEW_ID})
+      .expect(400));
   });
 
   after(()=> {
