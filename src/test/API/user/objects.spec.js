a9b1265d9466dd1fdd1e6986523354b8a3c3861b
sklyukin <stas.msu@gmail.com>
2016-10-04 16:02:17 +0500

    save and restore data for objects.spec.js

--- src/test/API/user/objects.spec.js
+++ src/test/API/user/objects.spec.js
@@ -5,7 +5,7 @@ import _ from 'lodash';
 let should = require('chai').should();
 import OBJECTS from  '../../fixtures/fObject.js';
 
-const {User} = app.models;
+const {User, UserExtra} = app.models;
 
 describe(`/users/:id/objects`, function () {
   updateTimeouts(this);
@@ -18,14 +18,13 @@ describe(`/users/:id/objects`, function () {
     return `users/${id}/objectsIds`;
   }
 
+  let idsBefore1, idsBefore2;
+
   before(function*() {
-    let {agent} = yield (user1Promise);
-    return yield (agent.get(_getUrl(user1.id))
-      .expect(200)
-      .expect(res => {
-        let ids = res.body;
-        ids.length.should.eq(0);
-      }));
+    idsBefore1 = yield (UserExtra.getObjectsIds(user1.id));
+    idsBefore2 = yield (UserExtra.getObjectsIds(user2.id));
+    yield (UserExtra.updateValue(user1.id, 'objects', []));
+    yield (UserExtra.updateValue(user2.id, 'objects', []));
   });
 
   describe('GET', () => {
@@ -41,118 +40,99 @@ describe(`/users/:id/objects`, function () {
         .expect(200));
     });
 
-    it('Sales - allow access to anyone list', () => {
-      return salesPromise.then(({agent}) => {
-        return agent.get(_getUrl(user1.id))
-          .expect(200);
-      });
+    it('Sales - allow access to anyone list', function* () {
+      let {agent} = yield (salesPromise);
+      yield (agent.get(_getUrl(user1.id))
+        .expect(200));
     });
 
-    it('Super - allow access to anyone list', () => {
-      return superPromise.then(({agent}) => {
-        return agent.get(_getUrl(user1.id))
-          .expect(200);
-      });
-    })
+    it('Super - allow access to anyone list', function* () {
+      let {agent} = yield (superPromise);
+      yield (agent.get(_getUrl(user1.id))
+        .expect(200));
+    });
   });
 
   describe('PUT', () => {
-    it('Anonymous - deny', () => {
-      return api.put(_url(user1.id))
+    it('Anonymous - deny', function* () {
+      yield (api.put(_url(user1.id))
         .send({objectId: OBJECTS.obj1.id})
-        .expect(401)
+        .expect(401));
     });
 
-    it('User - deny to link himself', () => {
-      return user1Promise.then(({agent}) => {
-        return agent.put(_url(user1.id))
-          .send({objectId: OBJECTS.obj1.id})
-          .expect(403)
-      })
+    it('User - deny to link himself', function* () {
+      let {agent} = yield (user1Promise);
+      yield (agent.put(_url(user1.id))
+        .send({objectId: OBJECTS.obj1.id})
+        .expect(403));
     });
 
-    it('Admin - deny to link himself', () => {
-      return adminPromise.then(({agent}) => {
-        return agent.put(_url(admin.id))
-          .send({objectId: OBJECTS.obj1.id})
-          .expect(403)
-      })
+    it('Admin - deny to link himself', function* () {
+      let {agent} = yield (adminPromise);
+      yield (agent.put(_url(admin.id))
+        .send({objectId: OBJECTS.obj1.id})
+        .expect(403));
     });
 
-    it('Super - allow to link anyone, then object owner will be able to add another user', () => {
-      return superPromise.then(({agent}) => {
-          return agent.put(_url(user1.id))
-            .send({objectId: OBJECTS.obj1.id})
-            .expect(200)
-        })
-        .then(() => {
-          return user1Promise.then(({agent}) => {
-            return agent.put(_url(user2.id))
-              .send({objectId: OBJECTS.obj1.id})
-              .expect(200)
-          })
-        })
-        .then(() => {
-          return superPromise.then(({agent}) => {
-            return agent.get(_getUrl(user2.id))
-              .expect(200)
-              .expect(res => {
-                let objects = res.body;
-                objects.should.include(OBJECTS.obj1.id);
-              })
-          })
-        })
+    it('Super - allow to link anyone, then object owner will be able to add another user', function* () {
+      let {agent} = yield (superPromise);
+      yield (agent.put(_url(user1.id))
+        .send({objectId: OBJECTS.obj1.id})
+        .expect(200));
+      agent = (yield (user1Promise)).agent;
+      yield (agent.put(_url(user2.id))
+        .send({objectId: OBJECTS.obj1.id})
+        .expect(200));
+      agent = (yield (superPromise)).agent;
+      yield (agent.get(_getUrl(user2.id))
+        .expect(200)
+        .expect(res => {
+          let objects = res.body;
+          objects.should.include(OBJECTS.obj1.id);
+        }));
     });
   });
 
   describe('DELETE', () => {
-    it('Anonymous - deny', () => {
-      return api.del(_url(user1.id))
+    it('Anonymous - deny', function* () {
+      yield (api.del(_url(user1.id))
         .send({objectId: OBJECTS.obj1.id})
-        .expect(401)
+        .expect(401));
     });
 
-    it('Super - allow to unlink anyone', () => {
-      return superPromise.then(({agent}) => {
-        return agent.del(_url(user1.id))
-          .send({objectId: OBJECTS.obj1.id})
-          .expect(200)
-          .expect(res => {
-            let objects = res.body;
-            let obj1 = objects.find(id => OBJECTS.obj1.id);
-            should.not.exist(obj1);
-          })
-      })
+    it('Super - allow to unlink anyone', function*() {
+      let {agent} = yield (superPromise);
+      return (agent.del(_url(user1.id))
+        .send({objectId: OBJECTS.obj1.id})
+        .expect(200)
+        .expect(res => {
+          let objects = res.body;
+          let obj1 = objects.find(id => OBJECTS.obj1.id);
+          should.not.exist(obj1);
+        }));
     });
 
-    it('User - deny to unlink if not owner', () => {
-      return user1Promise.then(({agent}) => {
-        return agent.del(_url(user1.id))
-          .send({objectId: OBJECTS.obj1.id})
-          .expect(403)
-      })
+    it('User - deny to unlink if not owner', function*() {
+      let {agent} = yield (user1Promise);
+      yield (agent.del(_url(user1.id))
+        .send({objectId: OBJECTS.obj1.id})
+        .expect(403));
     });
 
-    it('User - allow to unlink if owner', () => {
-      return superPromise.then(({agent}) => {
-          return agent.put(_url(user1.id))
-            .send({objectId: OBJECTS.obj1.id})
-            .expect(200)
-        })
-        .then(() => {
-          return user1Promise.then(({agent}) => {
-            return agent.del(_url(user1.id))
-              .send({objectId: OBJECTS.obj1.id})
-              .expect(200)
-          })
-        })
+    it('User - allow to unlink if owner', function*() {
+      let {agent} = yield (superPromise);
+      yield (agent.put(_url(user1.id))
+        .send({objectId: OBJECTS.obj1.id})
+        .expect(200));
+      agent = (yield (user1Promise)).agent;
+      yield (agent.del(_url(user1.id))
+        .send({objectId: OBJECTS.obj1.id})
+        .expect(200));
     });
+
+    after(function*() {
+      yield (UserExtra.updateValue(user1.id, 'objects', idsBefore1));
+      yield (UserExtra.updateValue(user2.id, 'objects', idsBefore2));
+    })
   });
-
-  after(function*() {
-    let promises = [];
-    promises.push(User.updateExtraValue(user1.id, 'objects', []));
-    promises.push(User.updateExtraValue(user2.id, 'objects', []));
-    return yield (promises);
-  })
 });
