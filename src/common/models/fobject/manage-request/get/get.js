857d097e831c18bc9911d21ffae02673254f932d
sklyukin <stas.msu@gmail.com>
2016-10-08 09:22:34 +0500

    manage requests count

--- src/common/models/fobject/manage-request/get/get.js
+++ src/common/models/fobject/manage-request/get/get.js
@@ -1,43 +1,68 @@
-import {App} from '../../../../services/App';
+import {App} from '../../../../services/App.js';
+import {ERRORS} from '../../../../utils/errors.js';
 import _ from 'lodash';
 
 export function initGet(ManageRequest) {
   ManageRequest.customFind = customFind;
+  ManageRequest.customCount = customCount;
 
   ManageRequest.remoteMethod(
     'customFind',
     {
       http: {path: '/', verb: 'get'},
-      description: `Find all ManageRequests matched by filter,
-        by default with '${ManageRequest.STATUS.ACTIVE}' status.`,
+      description: `Найти все ManageRequests по фильтру,
+        по умолчанию со статусом - '${ManageRequest.STATUS.ACTIVE}'.`,
       accessType: 'READ',
       accepts: {
         arg: 'filter',
         type: 'object',
-        description: 'Filter defining fields, where, include, order, offset, and limit'
+        description: 'Фильтр определяющий fields, where, include, order, offset, and limit'
       },
       returns: {root: true}
     }
   );
 
-  function customFind(filter) {
-    //should be at top, because userId can be lost, loopback issue
+  ManageRequest.remoteMethod(
+    'customCount',
+    {
+      http: {path: '/count', verb: 'get'},
+      description: `Количество ManageRequests удовлетваряющих where,
+        по умолчанию со статусом - '${ManageRequest.STATUS.ACTIVE}'.`,
+      accessType: 'READ',
+      accepts: {
+        arg: 'where',
+        type: 'object',
+        description: 'Фильтр определяющий критерий выборки'
+      },
+      returns: {root: true}
+    }
+  );
+
+  function* customFind(filter = {}) {
+    // should be at top, because userId can be lost, loopback issue
     let userId = App.getCurrentUserId();
-    return App.isSales()
-      .then(isSales => {
-        let status = _.get(filter, 'where.status', ManageRequest.STATUS.ACTIVE).toString();
-        if (isSales) {
-          filter = filter ? filter : {};
-          filter.where = filter.where ? filter.where : {};
-          filter.where.status = status;
-          return ManageRequest.find(filter);
-        }
-        let where = {userId: userId, status: status};
-        let subjectId = _.get(filter, 'where.subjectId');
-        if (subjectId) {
-          where.subjectId = subjectId.toString();
-        }
-        return ManageRequest.find({where: where});
-      })
+    let isSales = yield (App.isSales());
+    let status = _.get(filter, 'where.status', ManageRequest.STATUS.ACTIVE).toString();
+    if (isSales) {
+      filter.where = filter.where ? filter.where : {};
+      filter.where.status = status;
+      return yield (ManageRequest.find(filter));
+    }
+    let where = {userId: userId, status: status};
+    let subjectId = _.get(filter, 'where.subjectId');
+    if (subjectId) {
+      where.subjectId = subjectId.toString();
+    }
+    return yield (ManageRequest.find({where: where}));
+  }
+
+  function* customCount(where = {}) {
+    // should be at top, because userId can be lost, loopback issue
+    let userId = App.getCurrentUserId();
+    let isSales = yield (App.isSales());
+    if (!isSales)
+      throw ERRORS.forbidden('Доступ только сотрудникам');
+    where.status = _.get(where, 'status', ManageRequest.STATUS.ACTIVE).toString();
+    return yield (ManageRequest.count(where));
   }
 }
--- src/test/API/fobject/manage-request/general.spec.js
+++ src/test/API/fobject/manage-request/general.spec.js
@@ -23,133 +23,141 @@ describe(`/${COLLECTION_URL}/@general`, function () {
     subjectId: OBJECT_WITHOUT_MR.id
   };
 
-  it('Anonymous - deny to list manage requests', () => {
-    return api.get(COLLECTION_URL)
+  it('Anonymous - deny to list manage requests', function*() {
+    yield (api.get(COLLECTION_URL)
       .send(NEW_REQUEST)
-      .expect(401)
+      .expect(401));
   });
 
-  it('Anonymous - deny to create manage request', () => {
-    return api.post(COLLECTION_URL)
+  it('Anonymous - deny to count', function* () {
+    yield (api.get(`${COLLECTION_URL}/count`)
+      .expect(401));
+  });
+
+  it('Anonymous - deny to create manage request', function* () {
+    yield (api.post(COLLECTION_URL)
       .send(NEW_REQUEST)
-      .expect(401)
+      .expect(401));
   });
 
-
-  it('User1 - subjectId is required', () => {
-    return user1Promise.then(({agent})=> {
-      return agent.post(COLLECTION_URL)
-        .send(_.omit(NEW_REQUEST, 'subjectId'))
-        .expect(400)
-    });
+  it('User1 - subjectId is required', function* () {
+    let {agent} = yield (user1Promise);
+    yield (agent.post(COLLECTION_URL)
+      .send(_.omit(NEW_REQUEST, 'subjectId'))
+      .expect(400));
   });
 
-  it('User1 - should be able to create manage request', () => {
-    return user1Promise.then(({agent})=> {
-        return agent.post(COLLECTION_URL)
-          .send(NEW_REQUEST)
-          .expect(200)
-          .expect((res) => {
-            let request = res.body;
-            requestsIds.push(request.id);
-            request.status.should.eq(ManageRequest.STATUS.ACTIVE);
-            request.name.should.eq(NEW_REQUEST.name);
-            request.position.should.eq(NEW_REQUEST.position);
-            request.phone.should.eq(NEW_REQUEST.phone);
-            request.email.should.eq(NEW_REQUEST.email);
-            request.subjectId.should.eq(OBJECT_WITHOUT_MR.id);
-          })
-      })
-      .then(() => {
-        //should not be able to create request for the same object twice
-        return user1Promise.then(({agent})=> {
-          return agent.post(COLLECTION_URL)
-            .send(NEW_REQUEST)
-            .expect(400)
-        });
-      })
+  it('User1 - should be able to create manage request', function*() {
+    let {agent} = yield (user1Promise);
+    yield (agent.post(COLLECTION_URL)
+      .send(NEW_REQUEST)
+      .expect(200)
+      .expect((res) => {
+        let request = res.body;
+        requestsIds.push(request.id);
+        request.status.should.eq(ManageRequest.STATUS.ACTIVE);
+        request.name.should.eq(NEW_REQUEST.name);
+        request.position.should.eq(NEW_REQUEST.position);
+        request.phone.should.eq(NEW_REQUEST.phone);
+        request.email.should.eq(NEW_REQUEST.email);
+        request.subjectId.should.eq(OBJECT_WITHOUT_MR.id);
+      }));
+    // should not be able to create request for the same object twice
+    agent = (yield (user1Promise)).agent;
+    yield (agent.post(COLLECTION_URL)
+      .send(NEW_REQUEST)
+      .expect(400));
   });
 
-  it('User1 should be able to get his request', () => {
-    return user1Promise.then(({agent})=> {
-      return agent.get(COLLECTION_URL)
-        .expect(200)
-        .expect((res) => {
-          let requests = res.body;
-          let req = requests.find(req => req.userId === user1.id);
-          let otherStatus = requests.find(req => req.status !== ManageRequest.STATUS.ACTIVE);
-          should.exist(req);
-          should.not.exist(otherStatus);
-        })
-    });
+  it('User1 should be able to get his request', function*() {
+    let {agent} = yield (user1Promise);
+    yield (agent.get(COLLECTION_URL)
+      .expect(200)
+      .expect((res) => {
+        let requests = res.body;
+        let req = requests.find(req => req.userId === user1.id);
+        let otherStatus = requests.find(req => req.status !== ManageRequest.STATUS.ACTIVE);
+        should.exist(req);
+        should.not.exist(otherStatus);
+      }))
   });
 
-  it('User1 should be able to get his request by subjectId', () => {
-    return user1Promise.then(({agent})=> {
-      return agent.get(COLLECTION_URL)
-        .query({filter: {where: {subjectId: OBJECT1.id}}})
-        .expect(200)
-        .expect((res) => {
-          let requests = res.body;
-          let req = requests.find(req => req.userId === user1.id);
-          let otherStatus = requests.find(req => req.status !== ManageRequest.STATUS.ACTIVE);
-          should.exist(req);
-          should.not.exist(otherStatus);
-        })
-    });
+  it('User1 should be able to get his request by subjectId', function* () {
+    let {agent} = yield (user1Promise);
+    yield (agent.get(COLLECTION_URL)
+      .query({filter: {where: {subjectId: OBJECT1.id}}})
+      .expect(200)
+      .expect((res) => {
+        let requests = res.body;
+        let req = requests.find(req => req.userId === user1.id);
+        let otherStatus = requests.find(req => req.status !== ManageRequest.STATUS.ACTIVE);
+        should.exist(req);
+        should.not.exist(otherStatus);
+      }));
   });
 
-  it('User1 should be able to get his approved request', () => {
-    return user1Promise.then(({agent})=> {
-      return agent.get(COLLECTION_URL)
-        .send({filter: {where: {status: ManageRequest.STATUS.APPROVED}}})
-        .expect(200)
-        .expect((res) => {
-          let requests = res.body;
-          let approved = requests.find(req => req.status === ManageRequest.STATUS.APPROVED);
-          let not_approved = requests.find(req => req.status !== ManageRequest.STATUS.APPROVED);
-          should.exist(approved);
-          should.not.exist(not_approved);
-        })
-    });
+  it('User1 should be able to get his approved request', function* () {
+    let {agent} = yield (user1Promise);
+    yield (agent.get(COLLECTION_URL)
+      .send({filter: {where: {status: ManageRequest.STATUS.APPROVED}}})
+      .expect(200)
+      .expect((res) => {
+        let requests = res.body;
+        let approved = requests.find(req => req.status === ManageRequest.STATUS.APPROVED);
+        let not_approved = requests.find(req => req.status !== ManageRequest.STATUS.APPROVED);
+        should.exist(approved);
+        should.not.exist(not_approved);
+      }));
   });
 
-  it('User2 should not be able to get another\'s request', () => {
-    return user2Promise.then(({agent})=> {
-      return agent.get(COLLECTION_URL)
-        .expect(200)
-        .expect((res) => {
-          let requests = res.body;
-          let req = requests.find(req => req.userId !== user2.id);
-          should.not.exist(req);
-        })
-    });
+  it('User1 should not be able to count', function* () {
+    let {agent} = yield (user1Promise);
+    yield (agent.get(`${COLLECTION_URL}/count`)
+      .expect(401));
   });
 
-  it('Admin should not be able to get another\'s request', () => {
-    return adminPromise.then(({agent})=> {
-      return agent.get(COLLECTION_URL)
-        .expect(200)
-        .expect((res) => {
-          let requests = res.body;
-          let req = requests.find(req => req.userId !== admin.id);
-          should.not.exist(req);
-        })
-    });
+  it('User2 should not be able to get another\'s request', function*() {
+    let {agent} = yield (user2Promise);
+    yield (agent.get(COLLECTION_URL)
+      .expect(200)
+      .expect((res) => {
+        let requests = res.body;
+        let req = requests.find(req => req.userId !== user2.id);
+        should.not.exist(req);
+      }));
   });
 
-  it('Super should be able to get another\'s request', () => {
-    return superPromise.then(({agent})=> {
-      return agent.get(COLLECTION_URL)
-        .expect(200)
-        .expect((res) => {
-          let requests = res.body;
-          let req = requests.find(req => req.userId !== superUser.id);
-          should.exist(req);
-        })
-    });
+  it('Admin should not be able to get another\'s request', function* () {
+    let {agent} = yield (adminPromise);
+    yield (agent.get(COLLECTION_URL)
+      .expect(200)
+      .expect((res) => {
+        let requests = res.body;
+        let req = requests.find(req => req.userId !== admin.id);
+        should.not.exist(req);
+      }));
   });
 
+  it('Super should be able to get another\'s request', function* () {
+    let {agent} = yield (superPromise);
+    yield (agent.get(COLLECTION_URL)
+      .expect(200)
+      .expect((res) => {
+        let requests = res.body;
+        let req = requests.find(req => req.userId !== superUser.id);
+        should.exist(req);
+      }));
+  });
+
+  it('Super should be able to count', function* () {
+    let {agent} = yield (superPromise);
+    yield (agent.get(`${COLLECTION_URL}/count`)
+      .expect(200)
+      .expect((res) => {
+        let count = res.body;
+        count.should.least(2);
+      }));
+  });
 
   after(() => ManageRequest.deleteAll({'id': {inq: requestsIds}}))
 });
