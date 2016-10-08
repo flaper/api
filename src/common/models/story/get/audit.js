c2a4221be812c776b73f3bc8507aee9ff7d8250a
sklyukin <stas.msu@gmail.com>
2016-10-08 23:30:29 +0500

    count and filter for audit

--- src/common/models/story/get/audit.js
+++ src/common/models/story/get/audit.js
@@ -1,22 +1,4 @@
+import {disableRemoteRelation} from '../../core/common.js';
 export function initAuditRest(Story) {
-  Story.getAudit = getAudit;
-
-  Story.remoteMethod(
-    'getAudit',
-    {
-      http: {path: '/:id/audit', verb: 'get'},
-      description: `Возвращает историю изменений`,
-      accessType: 'READ',
-      accepts: {
-        arg: 'id',
-        type: 'string'
-      },
-      returns: {root: true}
-    }
-  );
-
-  function* getAudit(id) {
-    let {Audit} = Story.app.models;
-    return yield Audit.find({where: {subjectId: id}, order: 'created DESC'});
-  }
+  disableRemoteRelation(Story, 'audit', ['get', 'count']);
 }
