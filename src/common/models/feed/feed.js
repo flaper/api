34678266b0e58f42a6e4235cca36aba204991e19
HopeBetrayer <me.at.the.wind@gmail.com>
2016-11-02 18:09:36 +0300

    implemented Feed as model, added sort test

---
+++ src/common/models/feed/feed.js
@@ -0,0 +1,100 @@
+import RSS from "rss";
+import _ from 'lodash';
+
+module.exports = (Feed) => {
+  const ALLOWED_MODELS = ['Story'];
+  const MAX_FEED_LENGTH = 100;
+  const FORMATS = ['rss','json'];
+  Feed.commonInit(Feed);
+  Feed.disableAllRemotesExcept(Feed, []);
+  Feed.getFeed = getFeed;
+
+  let rssfeed = new RSS({
+      title: 'Flap feed',
+      description: 'latest stories from flap',
+      feed_url: 'http://api.flaper.org/feed',
+      site_url: 'http://flaper.org',
+      image_url: 'http://flaper.org/favicon.ico',
+      docs: 'http://flaper.org/feed/docs',
+      managingEditor: 'Flaper staff',
+      webMaster: 'Flaper staff',
+      copyright: '2016 Flaper',
+      language: 'ru',
+      categories: ['reviews'],
+      ttl: '600',
+  });
+
+  Feed.remoteMethod(
+    'getFeed',
+    {
+      http: {path: '/', verb: 'get'},
+      description: `Возвращает последние Story с type = review и status = active.`,
+      accessType: 'READ',
+      accepts: [
+        {
+          arg: 'domain',
+          type: 'string',
+          description: 'mainDomain name for object related to reviews'
+        },
+        {
+          arg: 'region',
+          type: 'string',
+          description: 'region of object related to reviews'
+        },
+        {
+          arg: 'type',
+          type: 'string',
+          description: 'story type. default - all stories'
+        },
+        {
+          arg: 'format',
+          type: 'string',
+          description: 'output format, for now accepts "rss" value'
+        }
+      ],
+      returns: {root: true}
+    }
+  );
+  function formatFeed(feed,format) {
+    console.log(feed);
+    switch (format.trim().toLowerCase()) {
+      case "rss":
+          let cache = rssfeed.xml();
+          feed.forEach(item => {
+            let {title,contentHTML,rating,created,id,userId} = item;
+            if(cache.indexOf(id.toString()) === -1)
+            rssfeed.item({
+                title:  title,
+                description: contentHTML,
+                guid: id.toString(),
+                url: `http://flaper.org/s/${id}`, // link to the item
+                categories: ['review'], // optional - array of item categories
+                date: created, // any format that js Date can parse.
+                custom_elements: [
+                  {'rating': rating}
+                ]
+            });
+          })
+        return rssfeed.xml();
+      default:
+        return feed;
+    }
+  }
+  function* getFeed(domain,region,type,format) {
+    let Story = Feed.app.models.Story,
+        filter = {
+          order: "created DESC",
+          limit: MAX_FEED_LENGTH,
+          where: {}
+        };
+    format = format ? FORMATS.indexOf(format) !== -1 ? format : "json" : "json";
+    if (domain)
+      filter.where.domain = [domain];
+    if (region)
+      filter.where.region = region;
+    if (type)
+      filter.where.type = type;
+    let feed = yield Story.scopeActive(filter);
+    return formatFeed(feed, format);
+  }
+};
--- src/test/API/feed/feed.spec.js
+++ src/test/API/feed/feed.spec.js
@@ -1,53 +1,60 @@
 import {updateTimeouts} from '../timeout';
-
+import {api}  from '../../helpers/api.js';
 import app from '../../helpers/app';
-import supertest from 'supertest';
-import defaults from 'superagent-defaults';
 
-import STORIES from  '../../fixtures/story';
-const STORY1 = STORIES.with_location1;
-const STORY2 = STORIES.with_location2;
-const STORY3 = STORIES.with_location3;
-
-const PORT = app.get('port');
-const API_URL = `http://0.0.0.0:${PORT}/`;
-const COLLECTION_URL = 'feed';
+const COLLECTION_URL = 'Feed';
 
 let should = require('chai').should();
 let expect = require('chai').expect;
 let Story = app.models.Story;
-let api = supertest(API_URL);
 
 describe(`/${COLLECTION_URL}`, function () {
   updateTimeouts(this);
 
   describe('getting the feed', () => {
 
-    it('Anonymous should be able to get the feed', () => {
+    it('Anonymous should be able to get the feed', function* () {
+      let query = {type:"review",status:"active"},
+          count = yield Story.count(query);
       return api.get(COLLECTION_URL)
+        .query(query)
         .expect(200)
         .expect(res => {
           let stories = res.body;
-          expect(stories.length).to.be.equal(6);
+          expect(stories.length).to.be.equal(count);
         });
     });
-
-
-    it('user should be able to get reviews from selected location', () => {
+    it('Stories should be sorted by creation date', function* () {
+      let query = {type:"review",status:"active"},
+          count = yield Story.count(query);
       return api.get(COLLECTION_URL)
-      .query({region:"оренбург"})
+        .query(query)
+        .expect(200)
+        .expect(res => {
+          let stories = res.body;
+          expect(stories[1].created).to.be.at.most(stories[0].created);
+        });
+    })
+
+    it('user should be able to get reviews from selected location', function*(){
+      let query = {region:"оренбург",type:"review",status:"active"},
+          count = yield Story.count(query);
+      return api.get(COLLECTION_URL)
+      .query(query)
       .expect(res => {
         let stories = res.body;
-        expect(stories.length).to.be.equal(2);
+        expect(stories.length).to.be.equal(count);
       });
     })
 
-    it('user should be able to get reviews from selected domain', () => {
+    it('user should be able to get reviews from selected domain', function*(){
+      let query = {mainDomain:"еда",type:"review",status:"active",domain:"еда"},
+          count = yield Story.count(query);
       return api.get(COLLECTION_URL)
-      .query({mainDomain:"еда"})
+      .query(query)
       .expect(res => {
         let stories = res.body;
-        expect(stories.length).to.be.equal(2);
+        expect(stories.length).to.be.equal(count);
       });
     })
   })
