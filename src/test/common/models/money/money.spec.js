98214afc74b3757f8dad995e38c94e3e1258d909
HopeBetrayer <me.at.the.wind@gmail.com>
2016-11-22 15:56:10 +0300

    changed tests to generators

--- src/test/common/models/money/money.spec.js
+++ src/test/common/models/money/money.spec.js
@@ -6,24 +6,20 @@ let Transaction = app.models.Transaction;
 
 describe('models/money', function () {
   let transactionsBefore = null;
-  before(() => {
-    return Transaction.count({})
-      .then(number => transactionsBefore = number);
+  before(function*() {
+    let number = yield Transaction.count({});
+    transactionsBefore = number;
   });
 
-  it('Test payment', () => {
+  it('Test payment', function*() {
     let amount = 10;
-    return Account.payment(user1.id, user2.id, amount)
-      .then((accounts) => {
-        accounts.length.should.eq(2);
-        return Transaction.find({order: "created DESC"})
-      })
-      .then(transactions => {
-        transactions.length.should.eq(transactionsBefore + 1);
-        let t = transactions[0];//last transaction
-        t.fromId.should.eq(user1.id);
-        t.toId.should.eq(user2.id);
-        t.amount.should.eq(amount);
-      })
+    let accounts = yield Account.payment(user1.id, user2.id, amount);
+    accounts.length.should.eq(2);
+    let transactions = yield Transaction.find({order: "created DESC"});
+    transactions.length.should.eq(transactionsBefore + 1);
+    let t = transactions[0];//last transaction
+    t.fromId.should.eq(user1.id);
+    t.toId.should.eq(user2.id);
+    t.amount.should.eq(amount);
   });
 });
