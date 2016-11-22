1a22677196ef3fae4bc103bb2529ee69af872bb2
HopeBetrayer <me.at.the.wind@gmail.com>
2016-11-22 15:39:23 +0300

    changed test to generators

--- src/test/API/story/sanitize.spec.js
+++ src/test/API/story/sanitize.spec.js
@@ -17,48 +17,45 @@ describe(`/${COLLECTION_URL}/@sanitize`, function () {
     content: "Nice <b>content</b> for test"
   };
 
-  it('Add - small content length should be an error', () => {
-    return user1Promise.then(({agent}) => {
+  it('Add - small content length should be an error', function*() {
+    let {agent} = yield user1Promise;
       return agent.post(COLLECTION_URL)
         .send(NEW_STORY)
         .expect(400)
-    })
   });
 
-  it('Add - should be sanitized', () => {
+  it('Add - should be sanitized', function*() {
     NEW_STORY.content = Sanitize.fakerIncreaseAlphaLength(NEW_STORY.content, Story.MIN_LENGTH.article);
     let validContent = 'Nice content for test';
     validContent = Sanitize.fakerIncreaseAlphaLength(validContent, Story.MIN_LENGTH.article);
 
-    return user1Promise.then(({agent}) => {
-      return agent.post(COLLECTION_URL)
-        .send(NEW_STORY)
-        .expect(200)
-        .expect((res) => {
-          let story = res.body;
-          story.title.should.eq('New story for test');
-          story.content.should.eq(validContent);
-        })
-    })
+    let {agent} = yield user1Promise;
+    return agent.post(COLLECTION_URL)
+      .send(NEW_STORY)
+      .expect(200)
+      .expect((res) => {
+        let story = res.body;
+        story.title.should.eq('New story for test');
+        story.content.should.eq(validContent);
+      })
   });
 
-  it('Update - should be sanitized', () => {
+  it('Update - should be sanitized', function*() {
     let title2 = '</div> ter </div>x<br>';
     let content2 = '//// dd<script>xs';
     let validContent2 = '//// dd';
     content2 = Sanitize.fakerIncreaseAlphaLength(content2, Story.MIN_LENGTH.article);
     validContent2 = Sanitize.fakerIncreaseAlphaLength(validContent2, Story.MIN_LENGTH.article);
 
-    return user1Promise.then(({agent}) => {
-      return agent.put(`${COLLECTION_URL}/${NEW_STORY.id}`)
-        .send({title: title2, content: content2})
-        .expect(200)
-        .expect((res) => {
-          let story = res.body;
-          story.title.should.eq('ter x');
-          story.content.should.eq(validContent2);
-        })
-    })
+    let {agent} = yield user1Promise;
+    return agent.put(`${COLLECTION_URL}/${NEW_STORY.id}`)
+      .send({title: title2, content: content2})
+      .expect(200)
+      .expect((res) => {
+        let story = res.body;
+        story.title.should.eq('ter x');
+        story.content.should.eq(validContent2);
+      })
   });
 
   after(function*() {
