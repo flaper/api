4424fb97395739a2da1240af42ea22fe6680362d
HopeBetrayer <me.at.the.wind@gmail.com>
2016-11-22 14:51:38 +0300

    changed tests to generators

--- src/test/API/user/settings.spec.js
+++ src/test/API/user/settings.spec.js
@@ -25,46 +25,39 @@ describe(`/users/:id/settings`, function () {
   });
 
   describe('POST', () => {
-    it('User - deny to update another\'s settings', () => {
-      return adminPromise.then(({agent}) => {
-        return agent.post(_url(USER1.id) + '/' + UserSettings.NAMES.HIDE_SOCIAL_LINKS)
-          .send({value: false})
-          .expect(401)
-      })
+    it('User - deny to update another\'s settings', function*() {
+      let {agent} = yield adminPromise;
+      return agent.post(_url(USER1.id) + '/' + UserSettings.NAMES.HIDE_SOCIAL_LINKS)
+        .send({value: false})
+        .expect(401)
     });
 
-    it('User - allow to update my settings', () => {
-      return user1Promise.then(({agent}) => {
-          return agent.post(_url(USER1.id) + '/' + UserSettings.NAMES.HIDE_SOCIAL_LINKS)
-            .send({value: false})
-            .expect(200)
-            .expect((res) => {
-              let value = res.body;
-              should.exist(value);
-              value.should.eq('false');
-            })
-        })
-        .then(() => {
-          return user1Promise.then(({agent}) => {
-            return agent.get(_url(USER1.id))
-              .expect(200)
-              .expect((res) => {
-                let settings = res.body;
-                should.exist(settings);
-                let setting = settings[UserSettings.NAMES.HIDE_SOCIAL_LINKS];
-                should.exist(setting);
-                setting.should.eq(false);
-              })
-          })
-        })
+    it('User - allow to update my settings', function*() {
+      let {agent} = yield user1Promise;
+      yield agent.post(_url(USER1.id) + '/' + UserSettings.NAMES.HIDE_SOCIAL_LINKS)
+        .send({value: false})
+        .expect(200)
+        .expect((res) => {
+          let value = res.body;
+          should.exist(value);
+          value.should.eq('false');
+        });
+      yield agent.get(_url(USER1.id))
+        .expect(200)
+        .expect((res) => {
+          let settings = res.body;
+          should.exist(settings);
+          let setting = settings[UserSettings.NAMES.HIDE_SOCIAL_LINKS];
+          should.exist(setting);
+          setting.should.eq(false);
+        });
     });
 
-    it('User - deny to update wrong settings', () => {
-      return user1Promise.then(({agent}) => {
-        return agent.post(_url(USER1.id) + '/wrong-settings')
-          .send({value: false})
-          .expect(400)
-      })
+    it('User - deny to update wrong settings', function*() {
+      let {agent} = yield user1Promise;
+      return agent.post(_url(USER1.id) + '/wrong-settings')
+        .send({value: false})
+        .expect(400)
     });
 
     after(() => UserSettings.deleteAll())
