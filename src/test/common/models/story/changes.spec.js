27ecee071ce40f9a52b201e8ce0ce5848f242c0b
HopeBetrayer <me.at.the.wind@gmail.com>
2016-11-22 15:51:27 +0300

    changed tests to generators

--- src/test/common/models/story/changes.spec.js
+++ src/test/common/models/story/changes.spec.js
@@ -8,28 +8,22 @@ let STORY1 = STORIES.test1;
 describe(`models/story/@changes`, function () {
   let updated;
   let story;
-  before(() => {
-    return Story.findById(STORY1.id)
-      .then(s => {
-        story = s;
-        updated = s.updated;
-      });
+  before(function*() {
+    let s = yield Story.findById(STORY1.id)
+    story = s;
+    updated = s.updated;
   });
 
-  it("Story should prevent empty update", () => {
-    return Story.updateAll({id: STORY1.id}, {})
-      .then(() => Story.findById(STORY1.id))
-      .then((s) => {
-        s.updated.getTime().should.eq(story.updated.getTime())
-      })
+  it("Story should prevent empty update", function*() {
+    yield Story.updateAll({id: STORY1.id}, {});
+    let s = yield Story.findById(STORY1.id);
+    s.updated.getTime().should.eq(story.updated.getTime());
   });
 
-  it("Story change - updated field should be changed", () => {
-    return Story.updateAll({id: STORY1.id}, {title: 'just a change'})
-      .then(() => Story.findById(STORY1.id))
-      .then((s) => {
-        s.updated.getTime().should.above(story.updated.getTime())
-      })
+  it("Story change - updated field should be changed", function*() {
+    yield Story.updateAll({id: STORY1.id}, {title: 'just a change'});
+    let s = yield Story.findById(STORY1.id);
+    s.updated.getTime().should.above(story.updated.getTime());
   });
 
   after(()=> Story.updateAll({id: STORY1.id}, {title: STORY1.title}));
