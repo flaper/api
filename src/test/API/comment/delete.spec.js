dcf113f5e0aca80d4f1664f48ed437f38134087d
HopeBetrayer <me.at.the.wind@gmail.com>
2016-11-22 14:21:16 +0300

    changed test to generators

--- src/test/API/comment/delete.spec.js
+++ src/test/API/comment/delete.spec.js
@@ -16,57 +16,50 @@ let Comment = app.models.Comment;
 
 const COLLECTION_URL = 'comments';
 
-describe(`/${COLLECTION_URL}/DELETE`, function () {
+describe.only(`/${COLLECTION_URL}/DELETE`, function () {
   updateTimeouts(this);
-  it('Anonymous - deny to delete', () => {
-    return api.del(`${COLLECTION_URL}/${COMMENT1.id}`)
+  it('Anonymous - deny to delete', function* () {
+    return api.delete(`${COLLECTION_URL}/${COMMENT1.id}`)
       .expect(401)
   });
 
-  it('User - deny to foreign delete', () => {
-    return user2Promise.then(({agent}) => {
-      return agent.del(`${COLLECTION_URL}/${COMMENT1.id}`)
-        .expect(401)
+  it('User - deny to foreign delete', function* () {
+    let {agent} = yield user2Promise;
+    return agent.delete(`${COLLECTION_URL}/${COMMENT1.id}`)
+      .expect(401)
+  });
+
+  it('User - allow to delete his comment', function* () {
+    let oldStory = yield Story.findByIdRequired(COMMENT1.subjectId),
+        {agent} = yield user1Promise;
+    yield agent.delete(`${COLLECTION_URL}/${COMMENT1.id}`)
+    .expect(200)
+    .expect((res) => {
+      let comment = res.body;
+      Comment.STATUS.DELETED.should.equal(comment.status);
     });
+    let changedStory = yield Story.findByIdRequired(COMMENT1.subjectId);
+    changedStory.commentsNumber.should.eq(oldStory.commentsNumber - 1);
+    yield returnProperties(Comment, COMMENT1.id, {status: Comment.STATUS.ACTIVE});
+    yield Comment.iSyncSubject('Story', COMMENT1.subjectId);
   });
 
-  it('User - allow to delete his comment', () => {
-    let oldStory = null;
-    return Story.findByIdRequired(COMMENT1.subjectId)
-      .then(story => oldStory = story)
-      .then(() => {
-        return user1Promise.then(({agent}) => {
-          return agent.del(`${COLLECTION_URL}/${COMMENT1.id}`)
-            .expect(200)
-            .expect((res) => {
-              let comment = res.body;
-              Comment.STATUS.DELETED.should.equal(comment.status);
-            })
-        })
-      }).then(() =>  Story.findByIdRequired(COMMENT1.subjectId))
-      .then(story => story.commentsNumber.should.eq(oldStory.commentsNumber - 1))
-      .then(() => returnProperties(Comment, COMMENT1.id, {status: Comment.STATUS.ACTIVE}))
-      .then(() => Comment.iSyncSubject('Story', COMMENT1.subjectId))
+  it('User - deny to delete already deleted comment', function* () {
+    let {agent} = yield user1Promise;
+    return agent.delete(`${COLLECTION_URL}/${COMMENT_DELETED1.id}`)
+      .expect(404)
   });
 
-  it('User - deny to delete already deleted comment', () => {
-    return user1Promise.then(({agent}) => {
-      return agent.del(`${COLLECTION_URL}/${COMMENT_DELETED1.id}`)
-        .expect(404)
-    })
-  });
-
-  it('Admin - allow to delete any comment', () => {
-    return adminPromise.then(({agent}) => {
-        return agent.del(`${COLLECTION_URL}/${COMMENT1.id}`)
-          .expect(200)
-          .expect((res) => {
-            let comment = res.body;
-            Comment.STATUS.DELETED.should.equal(comment.status);
-          })
-      })
-      .then(() => returnProperties(Comment, COMMENT1.id, {status: Comment.STATUS.ACTIVE}))
-      .then(() => Comment.iSyncSubject('Story', COMMENT1.subjectId))
+  it('Admin - allow to delete any comment', function* () {
+    let {agent} = yield adminPromise;
+    yield agent.delete(`${COLLECTION_URL}/${COMMENT1.id}`)
+      .expect(200)
+      .expect((res) => {
+        let comment = res.body;
+        Comment.STATUS.DELETED.should.equal(comment.status);
+      });
+    yield returnProperties(Comment, COMMENT1.id, {status: Comment.STATUS.ACTIVE});
+    yield Comment.iSyncSubject('Story', COMMENT1.subjectId);
   });
 
   const NEW_COMMENT = {
@@ -75,38 +68,31 @@ describe(`/${COLLECTION_URL}/DELETE`, function () {
     subjectId: STORIES.withoutActiveComments.id
   };
 
-  it('Last active should equal to subject created date, when last comment was removed', () => {
+  it('Last active should equal to subject created date, when last comment was removed', function* () {
     let comment = null;
-    return Story.findByIdRequired(NEW_COMMENT.subjectId)
-      .then(story => {
-        story.commentsNumber.should.eq(0)
-      })
-      .then(() => user1Promise)
-      .then(({agent}) => {
-        return agent.post(`${COLLECTION_URL}`)
-          .send(NEW_COMMENT)
-          .expect(200)
-          .expect((res) => {
-            comment = res.body;
-            comment.created = new Date(comment.created);
-            Comment.STATUS.ACTIVE.should.equal(comment.status);
-          })
-      })
-      .then(() =>  Story.findByIdRequired(NEW_COMMENT.subjectId))
-      .then((story) => {
-        story.commentsNumber.should.eq(1);
-        story.lastActive.getTime().should.eq(comment.created.getTime());
-      })
-      .then(() => user1Promise)
-      .then(({agent}) => {
-        return agent.del(`${COLLECTION_URL}/${NEW_COMMENT.id}`)
-          .expect(200)
-      })
-      .then(() =>  Story.findByIdRequired(NEW_COMMENT.subjectId))
-      .then((story) => {
-        story.commentsNumber.should.eq(0);
-        story.lastActive.getTime().should.eq(story.created.getTime());
-      })
-      .then(() => Comment.deleteById(NEW_COMMENT.id))
+    let story = yield Story.findByIdRequired(NEW_COMMENT.subjectId),
+        {agent} = yield user1Promise;
+    story.commentsNumber.should.eq(0);
+
+    yield agent.post(`${COLLECTION_URL}`)
+      .send(NEW_COMMENT)
+      .expect(200)
+      .expect((res) => {
+        comment = res.body;
+        comment.created = new Date(comment.created);
+        Comment.STATUS.ACTIVE.should.equal(comment.status);
+      });
+
+    story = yield Story.findByIdRequired(NEW_COMMENT.subjectId);
+    story.commentsNumber.should.eq(1);
+    story.lastActive.getTime().should.eq(comment.created.getTime());
+
+    yield agent.delete(`${COLLECTION_URL}/${NEW_COMMENT.id}`)
+      .expect(200);
+
+    story = yield Story.findByIdRequired(NEW_COMMENT.subjectId);
+    story.commentsNumber.should.eq(0);
+    story.lastActive.getTime().should.eq(story.created.getTime());
+    yield Comment.deleteById(NEW_COMMENT.id);
   })
 });
