a5d03a1f5fc7858212ef985893e82018b5a37771
HopeBetrayer <me.at.the.wind@gmail.com>
2016-11-24 11:42:29 +0300

    added returnProperties

--- src/test/API/vote/vote.spec.js
+++ src/test/API/vote/vote.spec.js
@@ -1,10 +1,12 @@
 import {api, user1, user1Promise, user2, user2Promise, adminPromise} from '../../helpers/api';
 import {updateTimeouts} from '../timeout';
+import {returnProperties} from '../commonModel/helper';
 import app from '../../helpers/app';
 import POLLS from  "../../fixtures/poll";
 let should = require('chai').should();
 let expect = require('chai').expect;
-let Poll = app.models.Poll;
+let User = app.models.User,
+    Poll = app.models.Poll;
 const COLLECTION_URL = "Votes";
 
 describe(`/${COLLECTION_URL}`, function() {
@@ -31,12 +33,10 @@ describe(`/${COLLECTION_URL}`, function() {
   });
   describe('CREATE/DELETE', () => {
     before(function*() {
-      // let Vote = app.models.Vote;
-      // yield Vote.create({
-      //   userId:user1.id,
-      //   targetId:POLLS.voteActive.id,
-      //   answer:"1a1000000000000000001001"
-      // });
+      let user = yield User.findByIdRequired(user2.id);
+      user.storiesNumber = 6;
+      user.level = 3;
+      user.save({skipIgnore:{storiesNumber:true,level:true}});
     })
 
     it("Anonymous cant vote", function*(){
@@ -48,12 +48,15 @@ describe(`/${COLLECTION_URL}`, function() {
 
     it.skip("Users can vote/unvote", function*(){ //skipped due to currentUser.js issues
       let id = POLLS.voteActive.id,
-          {agent} = yield user1Promise;
+          {agent} = yield user2Promise;
       yield agent.post(`${COLLECTION_URL}/${id}`)
       .send({answer:"1a1000000000000000001001"})
       .expect(200);
       yield agent.delete(`${COLLECTION_URL}/${id}`)
       .expect(200);
+    })
+    after(function*(){
+      yield returnProperties(User, user2.id, {level:0,storiesNumber:3});
     })
   })
 })
